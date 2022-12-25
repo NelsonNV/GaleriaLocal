03f891b17255eb2bb6250bfa93c9a47c1b79e301
NelsonNV <nelsonluisnv2@gmail.com>
2022-12-25 15:10:18 -0300

    se crea el primer modelo de privado
    se mejora el nombre de las funciones
    
    se crea una variable para las extenciones

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__,template_folder="static/template")
+app = Flask(__name__, template_folder="static/template")
+extAdmitidas = ('.mp4', '.mkv', '.avi', '.webm')
 
 @app.route('/')
-def index():
+def fnIndex():
     # Obtén la lista de todos los archivos de video en el directorio
-    extensiones_admitidas = ('.mp4','.mpg', '.mkv', '.avi', '.webm')
-    videos = [f for f in os.listdir('static/videos') if os.path.splitext(f)[1] in extensiones_admitidas]
+    videos = [f for f in os.listdir('static/videos') if os.path.splitext(f)[1] in extAdmitidas]
     return render_template('index.html', videos=videos)
 
+@app.route('/privada', methods=['GET', 'POST'])
+def fnPrivada():
+    password = request.form['password']
+    # Comprueba si la contraseña es correcta
+    if password == 'password':  # Deja en blanco la contraseña por ahora
+        # Obtén la lista de todos los archivos de video en el directorio privado
+        videos = [f for f in os.listdir('static/videosPrivados') if os.path.splitext(f)[1] in extAdmitidas]
+        return render_template('privado.html', videos=videos)
+    else:
+        return 'Contraseña incorrecta'
+
 if __name__ == '__main__':
-    app.run(port=80, host='127.0.0.2',threaded=True)
+    app.run(port=80, host='127.0.0.2', threaded=True)
